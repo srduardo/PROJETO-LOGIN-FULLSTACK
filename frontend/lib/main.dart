904e6fb564fcef36a5f32db5037eacda663ca158
srduardo <eduardodev8@gmail.com>
2024-08-02 22:49:15 -0300

    Front-end das páginas de login e registro finalizadas!

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+
   runApp(const MyApp());
 }
 
@@ -13,6 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'REGISTER AND LOGIN',
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(65, 0, 0, 100),
         appBarTheme: const AppBarTheme(
           backgroundColor: Color.fromARGB(100, 0, 200, 255),
         ),
@@ -29,7 +36,6 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(65, 0, 0, 100),
       appBar: AppBar(
         title: const Text("Home Page", style: TextStyle(color: Colors.white, fontSize: 24)),
       ),
@@ -38,19 +44,214 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(minimumSize: const Size(350, 100), backgroundColor: Color.fromARGB(100, 0, 200, 255)),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+                style: ElevatedButton.styleFrom(minimumSize: const Size(350, 100), backgroundColor: const Color.fromARGB(100, 0, 200, 255)),
                 child: const Text('Login', style: TextStyle(fontSize: 24, color: Colors.white)),
             ),
             const SizedBox(height: 20),
+
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterPage()));
+              },
               style: ElevatedButton.styleFrom(minimumSize: const Size(350, 100),),
               child: const Text('Register', style: TextStyle(fontSize: 24)),
             ),
           ]
         )
       )
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processando Login...')),
+      );
+    }
+  }
+
+  bool _obscureText = true;
+
+  void _revealPassword() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Page", style: TextStyle(fontSize: 24, color: Colors.white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 45),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira um email.';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Por favor, insira um email válido.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 50),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  suffixIcon: IconButton(icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off, color: Colors.white), onPressed: _revealPassword),
+                ),
+                style: const TextStyle(color: Colors.white),
+                obscureText: _obscureText,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira uma senha.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(onPressed: _submit, child: const Text("Login"))
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processando registro...'))
+      );
+    }
+  }
+
+  bool _obscureText = true;
+
+  void _revealPassword() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register Page", style: TextStyle(fontSize: 24, color: Colors.white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 45),
+              TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(labelText: 'Username'),
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.name,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Insira um username.';
+                  } else if (value.length < 6) {
+                    return 'O username deve conter no mínimo 6 caracteres';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 50),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                style: const TextStyle(color: Colors.white),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value == null || value.isEmpty){
+                    return 'Por favor, insira um email.';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Por favor, insira um email válido.';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 50),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  suffixIcon: IconButton(
+                      onPressed: _revealPassword,
+                      icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off))
+                ),
+                style: const TextStyle(color: Colors.white),
+                obscureText: _obscureText,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira uma senha.';
+                  } else if (value.length < 6) {
+                    return 'A Senha deve conter no mínimo 6 caracteres';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(onPressed: _submit, child: const Text("Register"))
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
+class DataPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Data Page", style: TextStyle(color: Colors.white, fontSize: 24))),
 
     );
   }
